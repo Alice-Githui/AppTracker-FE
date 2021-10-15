4f06f7a380a068e0f4615d0bfc653ef48ebb375e
Alice-Githui <gnyokabi.alice@gmail.com>
2021-10-15 15:51:35 +0300

    create a form to submit a post request to add a new potential job application

--- src/app/components/potential/potential.component.ts
+++ src/app/components/potential/potential.component.ts
@@ -21,14 +21,25 @@ export class PotentialComponent implements OnInit {
     position:'',
     website:''
 
-  }
+  };
+  submitted = false;
 
   addNewJobPosition():void{
     const data={
       companyName: this.new_potential.CompanyName,
       position: this.new_potential.position,
       website: this.new_potential.website
-    }
+    };
+
+    this.potentialService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
